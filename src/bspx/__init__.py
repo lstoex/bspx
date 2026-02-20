b2a7b25679b83aa272b97ab04a8861d97f3a9383
Lennard Stoellger <Lennard.Stoellger@tum.de>
2026-02-20 18:18:25 +0000

    opt-in typechecking

--- src/bspx/__init__.py
+++ src/bspx/__init__.py
@@ -2,9 +2,13 @@
 
 from jaxtyping import install_import_hook
 
-with install_import_hook("bspx", "beartype.beartype"):
-    from .bsplines import BSpline
-    from .utils_static import get_knots_static
+##---------------Uncomment for runtime type checking --------
+# with install_import_hook("bspx", "beartype.beartype"):
+#     from .bsplines import BSpline
+#     from .utils_static import get_knots_static
+###---------------Use this for no runtime type checking (faster) --------
+from .bsplines import BSpline
+from .utils_static import get_knots_static
 
 __all__ = ("BSpline", "get_knots_static")
 __version__ = "0.0.0"
