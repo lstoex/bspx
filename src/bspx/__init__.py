c1cdfe838cc798efe7e096f35b27930f97effdeb
Lennard Stoellger <Lennard.Stoellger@tum.de>
2026-02-23 16:45:43 +0000

    Renaming

--- src/bspx/__init__.py
+++ src/bspx/__init__.py
@@ -3,10 +3,10 @@
 ##---------------Uncomment for runtime type checking --------
 # from jaxtyping import install_import_hook
 # with install_import_hook("bspx", "beartype.beartype"):
-#     from .bsplines import bspline, bspline_derivative
+#     from .curves import bspline, bspline_derivative
 #     from .utils_static import get_knots_static
 ###---------------Use this for no runtime type checking (faster) --------
-from .bsplines import bspline, bspline_derivative
+from .curves import bspline, bspline_derivative
 from .utils_static import get_knots_static
 
 __all__ = ("bspline", "get_knots_static", "bspline_derivative")
