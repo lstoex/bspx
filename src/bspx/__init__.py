ceb584957e9266fc150316fca06a624594282d60
Lennard Stoellger <Lennard.Stoellger@tum.de>
2026-03-03 11:14:27 +0000

    Fix: Imports for Knots

--- src/bspx/__init__.py
+++ src/bspx/__init__.py
@@ -3,14 +3,16 @@
 # ---------------Uncomment for runtime type checking --------
 from jaxtyping import install_import_hook
 
-with install_import_hook("bspx", "beartype.beartype"):
-    from .curves import bspline, bspline_derivative
+# with install_import_hook("bspx", "beartype.beartype"):
+#     from .curves import bspline, bspline_derivative
+#     from .utils import clamped_uniform_knot_vector
 ###---------------Use this for no runtime type checking (faster) --------
-# from .curves import bspline, bspline_derivative
-# from .utils_static import make_uniform_knot_vector_static
+from .curves import bspline, bspline_derivative
+from .utils import clamped_uniform_knot_vector
 
 __all__ = (
     "bspline",
     "bspline_derivative",
+    "clamped_uniform_knot_vector",
 )
 __version__ = "0.0.0"
